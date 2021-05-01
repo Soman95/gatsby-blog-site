8316a6bccb504a4a9872387854f8ba3968236a3f
Soman95 <s.o.ibrahim95@hotmal.com>
2021-05-01 17:52:39 +0100

    clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Sulayman, a front-end developer living in the vibrant city of London.</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </Layout>
   )
 }
